85995e70774a3a8ceecdf39e0831b8fe397b1667
Tsumiki <93415945+nhuhuynh1508@users.noreply.github.com>
2025-05-06 13:45:03 +0700

    Change background color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { Film } from "lucide-react";
+import './App.css';
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="min-h-screen bg-jet-black flex items-center justify-center">
+        <div className="grid min-h-screen lg:grid-cols-2 w-full">
+          <div className="flex flex-col gap-4 p-2 md:p-4">
+            <div className="flex justify-center gap-2 md:justify-start">
+              <a href="#" className="flex items-center gap-2 font-medium">
+                <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground text-white">
+                  <Film className="size-20" />
+                </div>
+                <div className="text-white text-2xl font-semibold font-sanss">CineBook</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
