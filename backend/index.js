1e461f0604fbe05a69a8763ee0f3856bafd47c99
kuuhakuXzy <hnguyen.nguyen01xzy@gmail.com>
2025-06-25 17:58:21 +0700

    Update final

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ dbconnect();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://movie-ticket-booking-1-w6kq.onrender.com'],
+  origin: ['http://localhost:5173', 'https://movie-ticket-booking-w348.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
